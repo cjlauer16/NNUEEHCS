64a48e6fd92b214f4f3ec66e68046eaf02034c70
Zane Fink <finkzane@gmail.com>
2024-09-12 16:17:58 -0500

    Driver prep for BO

--- examples/bo_driven/bo.py
+++ examples/bo_driven/bo.py
@@ -35,12 +35,66 @@ def get_model_builder_class(uq_method):
         return EnsembleModelBuilder
     elif uq_method == 'kde':
         return KDEModelBuilder
-    elif uq_method == 'delta':
+    elif uq_method == 'delta_uq':
         return DeltaUQMLPModelBuilder
     elif uq_method == 'pager':
         return PAGERModelBuilder
     else:
         raise ValueError(f'Unknown uq method {uq_method}')
+
+
+def build_model(model_cfg, uq_config, uq_method):
+    builder_class = get_model_builder_class(uq_method)
+    builder = builder_class(model_cfg['architecture'],
+                            uq_config[uq_method]
+                            )
+    return builder.build()
+
+
+def get_dataset(dataset_cfg, dataset_name, is_ood=False):
+    if is_ood:
+        ds_name = get_ood_dataset_name(dataset_name)
+    else:
+        ds_name = get_id_datset_name(dataset_name)
+    dset = get_dataset_from_config(dataset_cfg, ds_name)
+    return dset
+
+
+def prepare_dset_for_use(dset, training_cfg, scaling_dset=None):
+    ipt = dset.input
+    opt = dset.output
+    if scaling_dset is None:
+        scale_ipt = ipt
+        scale_opt = opt
+    else:
+        scale_ipt = scaling_dset.input
+        scale_opt = scaling_dset.output
+
+    if training_cfg['scaling'] is True:
+        # do min-max scaling to get it to 0-1
+        opt = (opt - scale_opt.min()) / (scale_opt.max() - scale_opt.min())
+        dset.output = opt
+        ipt = (ipt - scale_ipt.min()) / (scale_ipt.max() - scale_ipt.min())
+        dset.input = ipt
+    return dset
+
+
+def evaluate(model, id_dset, ood_dset):
+    id_ipt = id_dset.input.to(model.device)
+    id_opt = id_dset.output.to(model.device)
+    ood_ipt = ood_dset.input.to(model.device)
+    ood_opt = ood_dset.output.to(model.device)
+
+    indices = torch.randperm(id_ipt.size(0))
+    indices_ood = torch.randperm(ood_ipt.size(0))
+
+    id_ipt = id_ipt[indices][:20000]
+    id_opt = id_opt[indices][:20000]
+    ood_ipt = ood_ipt[indices_ood][:20000]
+    ood_opt = ood_opt[indices_ood][:20000]
+
+    eval = get_uncertainty_evaluator('wasserstein')
+    return eval.evaluate(model, (id_ipt, id_opt), (ood_ipt, ood_opt))
 
 
 @click.command()
@@ -57,70 +111,39 @@ def main(benchmark, uq_method, dataset):
         uq_config = config['uq_methods']
 
     name = benchmark
-    train_ds_name = get_id_datset_name(dataset)
 
-    dset = get_dataset_from_config(dataset_cfg, train_ds_name)
-    builder_class = get_model_builder_class(uq_method)
-    builder = builder_class(model_cfg['architecture'],
-                            uq_config[uq_method]
-                            )
-    model = builder.build().to(dset.dtype)
+    dset = get_dataset(dataset_cfg, dataset)
+    dset = prepare_dset_for_use(dset, training_cfg)
+    model = build_model(model_cfg, uq_config, uq_method).to(dset.dtype)
     trainer = get_trainer(trainer_cfg, name, model)
-
-    ipt = dset.input
-    opt = dset.output
-    # do min-max scaling to get it to 0-1
-    opt = (opt - opt.min()) / (opt.max() - opt.min())
-    dset.output = opt
-    ipt = (ipt - ipt.min()) / (ipt.max() - ipt.min())
-    dset.input = ipt
 
     train_dl = DataLoader(dset, batch_size=training_cfg['batch_size'], shuffle=True)
     test_dl = DataLoader(dset, batch_size=training_cfg['batch_size'], shuffle=False)
     trainer.fit(model, train_dl, test_dl)
-    torch.save(model, 'model.pth')
 
-    model = torch.load('logs/binomial_options/version_0/model.pth')
+    model = torch.load(f'{trainer.logger.log_dir}/model.pth')
 
     model.eval()
     with torch.no_grad():
-        id_dset_name = get_id_datset_name(dataset)
-        ood_dset_name = get_ood_dataset_name(dataset)
-        dset_id = get_dataset_from_config(dataset_cfg, id_dset_name)
-        dset_ood = get_dataset_from_config(dataset_cfg, ood_dset_name)
+        dset_id = get_dataset(dataset_cfg, dataset)
+        dset_ood = get_dataset(dataset_cfg, dataset, is_ood=True)
 
-        id_ipt = dset_id.input.to(model.device)
-        id_opt = dset_id.output.to(model.device)
+        # we have to scale ood relative to ID
+        # we MUST scale OOD first because this method
+        # modifies in place
+        dset_ood = prepare_dset_for_use(dset_ood, training_cfg,
+                                        scaling_dset=dset_id)
+        dset_id = prepare_dset_for_use(dset_id, training_cfg)
 
-        ood_ipt = dset_ood.input.to(model.device)
-        ood_opt = dset_ood.output.to(model.device)
-
-        # We need to normalize opt before normalizing ipt
-        ood_ipt = (ood_ipt - id_ipt.min()) / (id_ipt.max() - id_ipt.min())
-        ood_opt = (ood_opt - id_opt.min()) / (id_opt.max() - id_opt.min())
-
-        id_ipt = (id_ipt - id_ipt.min()) / (id_ipt.max() - id_ipt.min())
-        id_opt = (id_opt - id_opt.min()) / (id_opt.max() - id_opt.min())
-
-        indices = torch.randperm(id_ipt.size(0))
-        indices_ood = torch.randperm(ood_ipt.size(0))
-
-        id_ipt = id_ipt[indices][:20000]
-        id_opt = id_opt[indices][:20000]
-        ood_ipt = ood_ipt[indices_ood][:20000]
-        ood_opt = ood_opt[indices_ood][:20000]
-
-        eval = get_uncertainty_evaluator('wasserstein')
-        results = eval.evaluate(model, (id_ipt, id_opt), (ood_ipt, ood_opt))
-
+        results = evaluate(model, dset_id, dset_ood)
         print(results)
 
         id_ue = results['id_ue']
         ood_ue = results['ood_ue']
 
         fig, ax = plt.subplots()
-        ax.ecdf(id_ue.data.flatten(), label='ID')
-        ax.ecdf(ood_ue.data.flatten(), label='OOD')
+        ax.ecdf(id_ue.data[0].flatten(), label='ID')
+        ax.ecdf(ood_ue.data[0].flatten(), label='OOD')
         ax.legend()
         # save it to png file
         plt.savefig('uncertainty.png')
